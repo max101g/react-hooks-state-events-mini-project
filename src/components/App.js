405fc6ef8b7fa90df7964b71114780ba13753463
max101g <maxgithinji101@gmail.com>
2023-01-16 04:38:22 +0300

    update react

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,3 @@
-// import React from "react";
-// import CategoryFilter from "./CategoryFilter";
-// import NewTaskForm from "./NewTaskForm";
-// import TaskList from "./TaskList";
-
-// import { CATEGORIES, TASKS } from "../data";
-// console.log("Here's the data you're working with");
-// console.log({ CATEGORIES, TASKS });
-
-// function App({}) {
-
-//   return (
-//     <div className="App">
-//       <h2>My tasks</h2>
-//       <CategoryFilter />
-//       <NewTaskForm categories = {CATEGORIES} tasks = {TASKS}/>
-//       <TaskList />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React,{useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,3 @@
-// import React from "react";
-
-// function TaskList() {
-//   return (
-//     <div className="tasks">
-//       {/* display a list of tasks using Task component */}
-//     </div>
-//   );
-// }
-
-// export default TaskList;
-
 import React from "react";
 import Task from "./Task";
 
